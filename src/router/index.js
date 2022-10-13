592dec62121ea3ed40e76294a4d4f8399effd8c4
Alex <alejandro.aguila.27@icloud.com>
2022-10-13 10:28:00 -0500

    Corrección Index.js

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/loginView.vue')
+      component: () => import('../views/LoginView.vue')
     },
     {
       path: '/profile',
